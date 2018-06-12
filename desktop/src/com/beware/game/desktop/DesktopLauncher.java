7a3235318c8e22264b65562673fd0ad53208ec97
RoseauFragile <benjamin.coudannes@viacesi.fr>
2018-06-12 17:36:09 +0200

    d

---
+++ desktop/src/com/beware/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.beware.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.beware.game.gameBewareOfTheTruth;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new gameBewareOfTheTruth(), config);
+	}
+}
