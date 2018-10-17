86dc0c99a95369e5b50aa15f5b8286cd089d9e7b
RoseauFragile <benjamin.coudannes@viacesi.fr>
2018-10-17 21:27:29 +0200

    player Input ok

--- core/src/bewareofthetruth/entity/components/PlayerInputComponent.java
+++ core/src/bewareofthetruth/entity/components/PlayerInputComponent.java
@@ -1,17 +1,114 @@
 package bewareofthetruth.entity.components;
 
-public class PlayerInputComponent extends InputComponent {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.math.Vector3;
 
+public class PlayerInputComponent extends InputComponent implements InputProcessor {
+
+	private final static String TAG = PlayerInputComponent.class.getSimpleName();
+	private Vector3 _lastMouseCoordinates;
+	
+	public PlayerInputComponent() {
+		this._lastMouseCoordinates = new Vector3();
+		Gdx.input.setInputProcessor(this);
+	}
+	
 	@Override
 	public void dispose() {
 	}
 
 	@Override
 	public void receiveMessage(String message) {
+		String[] string = message.split(MESSAGE_TOKEN);
+		if(string.length == 0) return;
+		
+		//Specifically for messages with 1 object payload
+		if(string.length ==2) {
+			if(string[0].equalsIgnoreCase(MESSAGE.CURRENT_DIRECTION.toString())) {
+				_currentDirection = _json.fromJson(Entity.Direction.class, string[1]);
+			}
+		}
 	}
 
 	@Override
-	public void update(Entity entity, float delta) {
+	public void update(Entity entity, float delta){
+		//Keyboard input
+		/*if(keys.get(Keys.PAUSE)) {
+			MainGameScreen.setGameState(MainGameScreen.GameState.PAUSED);
+			pauseReleased();
+		}else*/ if( keys.get(Keys.LEFT)){
+			entity.sendMessage(MESSAGE.CURRENT_STATE, _json.toJson(Entity.State.WALKING));
+			entity.sendMessage(MESSAGE.CURRENT_DIRECTION, _json.toJson(Entity.Direction.LEFT));
+		}else if( keys.get(Keys.RIGHT)){
+			entity.sendMessage(MESSAGE.CURRENT_STATE, _json.toJson(Entity.State.WALKING));
+			entity.sendMessage(MESSAGE.CURRENT_DIRECTION, _json.toJson(Entity.Direction.RIGHT));
+		}else if( keys.get(Keys.UP)){
+			entity.sendMessage(MESSAGE.CURRENT_STATE, _json.toJson(Entity.State.WALKING));
+			entity.sendMessage(MESSAGE.CURRENT_DIRECTION, _json.toJson(Entity.Direction.UP));
+		}else if(keys.get(Keys.DOWN)){
+			entity.sendMessage(MESSAGE.CURRENT_STATE, _json.toJson(Entity.State.WALKING));
+			entity.sendMessage(MESSAGE.CURRENT_DIRECTION, _json.toJson(Entity.Direction.DOWN));
+		}else if(keys.get(Keys.QUIT)) {
+			quitReleased();
+			Gdx.app.exit();
+		}else{
+			entity.sendMessage(MESSAGE.CURRENT_STATE, _json.toJson(Entity.State.IDLE));
+			if( _currentDirection == null ){
+				entity.sendMessage(MESSAGE.CURRENT_DIRECTION, _json.toJson(Entity.Direction.DOWN));
+			}
+		}
+
+		//Mouse input
+		if( mouseButtons.get(Mouse.SELECT)) {
+			//Gdx.app.debug(TAG, "Mouse LEFT click at : (" + _lastMouseCoordinates.x + "," + _lastMouseCoordinates.y + ")" );
+			entity.sendMessage(MESSAGE.INIT_SELECT_ENTITY, _json.toJson(_lastMouseCoordinates));
+			mouseButtons.put(Mouse.SELECT, false);
+		}
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+	
+	public void quitReleased(){
+		keys.put(Keys.QUIT, false);
 	}
 
 }
